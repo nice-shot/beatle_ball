4bf1f874950ea56fea7686ddfaabec2edaaf2fca
nice-shot <shefferu@gmail.com>
2019-10-11 22:19:15 +0300

    Added force from ball to ground

--- Assets/Scripts/BallFollower.cs
+++ Assets/Scripts/BallFollower.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BallFollower : MonoBehaviour {
-    public GameObject ball;
+    public Rigidbody2D ball;
     public LayerMask collisionMask;
+    public float forceAmount;
 
     void Update() {
         transform.position = ball.transform.position;
@@ -13,6 +14,7 @@ public class BallFollower : MonoBehaviour {
         if (hit) {
             print("Hit: " + hit.normal.ToString());
             transform.up = hit.normal;
+            ball.AddForce(-transform.up * forceAmount);
         }
     }
 }
