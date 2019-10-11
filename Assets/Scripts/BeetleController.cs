20e380f1b4f9a5b86ba9d9f627f86022964b4cc6
nice-shot <shefferu@gmail.com>
2019-10-12 01:12:40 +0300

    Continued work on the rigidbody beetle

--- Assets/Scripts/BeetleController.cs
+++ Assets/Scripts/BeetleController.cs
@@ -5,14 +5,26 @@ using UnityEngine;
 public class BeetleController : MonoBehaviour
 {
     public float speed;
+    public LayerMask groundLayer;
+    public Transform groundCheck;
+
+    public bool isGrounded;
+
     Rigidbody2D rb;
+
 
     void Awake() {
         rb = GetComponent<Rigidbody2D>();
     }
 
     void Update() {
-        Vector2 velocity = transform.right * Input.GetAxis("Horizontal");
-        rb.velocity = velocity * speed * Time.deltaTime;
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.15f, groundLayer);
+
+        if (isGrounded) {
+            Vector2 direction = transform.right * Input.GetAxisRaw("Horizontal");
+            print("Changing velocity to: " + direction.ToString());
+            rb.velocity = direction * speed * Time.deltaTime;
+        }
+
     }
 }
