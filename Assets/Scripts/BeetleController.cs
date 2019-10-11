c56b227499c39b47dc05225ba15be962aa565537
nice-shot <shefferu@gmail.com>
2019-10-12 00:38:04 +0300

    Started new scene for controlling beatle with rigidbody

---
+++ Assets/Scripts/BeetleController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeetleController : MonoBehaviour
+{
+    public float speed;
+    Rigidbody2D rb;
+
+    void Awake() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        Vector2 velocity = transform.right * Input.GetAxis("Horizontal");
+        rb.velocity = velocity * speed * Time.deltaTime;
+    }
+}
