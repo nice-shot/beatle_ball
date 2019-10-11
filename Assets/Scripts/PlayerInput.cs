5aa85a9abe5cdaa12b6fbb2c1806aaad43dd91f0
nice-shot <shefferu@gmail.com>
2019-10-11 18:37:05 +0300

    Added sprite flipping

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -5,13 +5,17 @@ using System.Collections;
 public class PlayerInput : MonoBehaviour {
 
 	Player player;
+	SpriteRenderer spriteRenderer;
 
 	void Start () {
 		player = GetComponent<Player> ();
+		spriteRenderer = GetComponentInChildren<SpriteRenderer>();
 	}
 
 	void Update () {
 		Vector2 directionalInput = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
 		player.SetDirectionalInput (directionalInput);
+
+		spriteRenderer.flipX = Mathf.Sign(directionalInput.x) == -1;
 	}
 }
