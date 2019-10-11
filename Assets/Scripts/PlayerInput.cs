7022893bb9f4dd83d96d7c2dceb04c8e98317f71
nice-shot <shefferu@gmail.com>
2019-10-11 20:30:09 +0300

    Added simple sprite rotation

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -4,11 +4,15 @@ using System.Collections;
 [RequireComponent (typeof (Player))]
 public class PlayerInput : MonoBehaviour {
 
+	public float spriteRotationSmooth;
+
 	Player player;
+	Controller2D controller;
 	SpriteRenderer spriteRenderer;
 
 	void Start () {
-		player = GetComponent<Player> ();
+		player = GetComponent<Player>();
+		controller = GetComponent<Controller2D>();
 		spriteRenderer = GetComponentInChildren<SpriteRenderer>();
 	}
 
@@ -16,6 +20,21 @@ public class PlayerInput : MonoBehaviour {
 		Vector2 directionalInput = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
 		player.SetDirectionalInput (directionalInput);
 
-		spriteRenderer.flipX = Mathf.Sign(directionalInput.x) == -1;
+		CheckRotation();
+	}
+
+	void CheckRotation() {
+		spriteRenderer.flipX = controller.collisions.faceDir == -1;
+		Transform spriteTrans = spriteRenderer.transform;
+		RaycastHit2D hit = Physics2D.Raycast(transform.position, -spriteTrans.up, Mathf.Infinity, controller.collisionMask);
+		if (hit) {
+			spriteTrans.up = hit.normal;
+			// print(hit.normal);
+			// print(spriteTrans.up);
+			// Quaternion rot = Quaternion.FromToRotation(spriteTrans.up, hit.normal);
+			// rot *= Quaternion.Euler(0, spriteTrans.rotation.y, spriteTrans.rotation.z);
+			// print("Rotation: " + rot.ToString());
+			// spriteTrans.rotation = Quaternion.Lerp(spriteTrans.rotation, rot, Time.deltaTime * spriteRotationSmooth);;
+		}
 	}
 }
