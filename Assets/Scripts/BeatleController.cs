8f8889d076317fb26f8dedc09cb561ad69b39ebd
nice-shot <shefferu@gmail.com>
2019-10-11 16:09:04 +0300

    Playing with Rigidbody for beatle and ball

---
+++ Assets/Scripts/BeatleController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatleController : MonoBehaviour {
+    public float speed;
+    private Rigidbody2D rb;
+
+    void Awake() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        float movement = Input.GetAxis("Horizontal");
+        if (movement != 0) {
+            Vector2 newPosition = transform.position;
+            newPosition.x += movement * speed * Time.deltaTime;
+            rb.MovePosition(newPosition);
+        }
+    }
+}
