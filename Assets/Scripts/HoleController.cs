9a9bb5b02ee76f0ff3d6f3ef3012c65e72cd35a4
ido.lazer <ido.lazer@mail.huji.ac.il>
2019-10-12 17:19:52 +0300

    Added hole

---
+++ Assets/Scripts/HoleController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoleController : MonoBehaviour
+{
+    [SerializeField] Collector collector;
+    
+    void Start()
+    {
+        GetComponent<Collider2D>().enabled = false;
+        GetComponent<BoxCollider2D>().enabled = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (collector.Size == 8)
+        {
+            GetComponent<PolygonCollider2D>().enabled = true;
+            GetComponent<BoxCollider2D>().enabled = true;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Ball"))
+        {
+            var sprite = col.GetComponentInChildren<SpriteRenderer>();
+            sprite.sortingOrder = 1;
+        }
+    }
+}
